101d0a0f9bec3955eade4d722f59e94c057d4d1e
hans-kevin <60101376+hans-kevin@users.noreply.github.com>
2021-05-26 12:19:44 +0200

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -21,16 +21,19 @@ CREATE TABLE Parking
  ,ExitDate DATETIME
 );
 
+/*
 DROP TRIGGER tr_Parking;
+*/
+
 DELIMITER $$
 CREATE TRIGGER tr_Parking
 BEFORE INSERT ON Parking 
 FOR EACH ROW
 BEGIN
-  IF NEW.SeasonTicketId IS NOT NULL OR NOT EXISTS (SELECT * FROM SeasonTicket AS ST WHERE ST.SeasonTicketID = NEW.SeasonTicketID AND CURRENT_DATE() BETWEEN ST.ValidFrom AND ST.ValidTo)
+  IF NEW.SeasonTicketId IS NOT NULL OR NOT EXISTS (SELECT * FROM SeasonTicket AS ST WHERE ST.SeasonTicketID = NEW.SeasonTicketID AND CURRENT_DATE() BETWEEN ST.ValidFrom AND ST.ValidTo) /* checks if the seasonticket id is valid */
   THEN SIGNAL SQLSTATE '45000' 
 	   SET MESSAGE_TEXT = "This is not a valid seasonticket.";
-  ELSEIF EXISTS (SELECT * FROM Parking AS P WHERE P.LicensePlate = NEW.LicensePlate AND P.ExitDate IS NULL)  
+  ELSEIF EXISTS (SELECT * FROM Parking AS P WHERE P.LicensePlate = NEW.LicensePlate AND P.ExitDate IS NULL) AND NEW.ExitDate IS NULL /*check at entry if already a car with that licenseplate parks in the parkinglot */ 
   THEN SIGNAL SQLSTATE '45000' 
 	   SET MESSAGE_TEXT = "You're already parking in our lot.";  
   END IF;
