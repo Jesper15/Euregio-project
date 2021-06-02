7cfc197f5b214a44cc88f287d531262635c9d7fa
jesper.otten <230717@edu.rocfriesepoort.nl>
2021-06-02 12:59:52 +0200

    Connected the form to the database

--- include/send.php
+++ include/send.php
@@ -4,4 +4,14 @@ $SeasonTicketID = $_POST['seasonticket'];
 
 require 'db.php';
 
-$send = "insert into parking (licenseplate, seasonticket) values ('$LicensePlate', '$SeasonTicketID')";
+$send = "insert into parking.parking (LicensePlate, SeasonTicketID) values ('$LicensePlate', '$SeasonTicketID')";
+
+if($conn->query($send)) {
+    header('Location: ../forms.php');
+
+}
+else
+{
+    echo "er is iets foutgegaan" . $conn->error;
+}
+$conn->close();
