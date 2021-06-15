eb3737873e3c29a4f112b287ed984e4f1474ab2c
jesper.otten <230717@edu.rocfriesepoort.nl>
2021-06-15 13:34:30 +0200

    Added some comments

--- include/send.php
+++ include/send.php
@@ -1,11 +1,18 @@
 <?php
+
+//Defining the variables of the form and database
+
 $LicensePlate = $_POST['lplate'];
 $SeasonTicketID = $_POST['sticket'];
 $EntryDate = $_POST['Entry-date'];
 
 require 'db.php';
 
+//Insert into the database
+
 $send = "insert into parking.parking (LicensePlate, SeasonTicketID, EntryDate) values ('$LicensePlate', '$SeasonTicketID', '$EntryDate')";
+
+//Check if the credentials are right. Or else, give an error
 
 if($conn->query($send)) {
     header('Location: ../index.php');
