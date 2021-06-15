dc2fd66bfefbf4b5230fa4af838d4d3c4f95d9fe
jesper.otten <230717@edu.rocfriesepoort.nl>
2021-06-15 10:09:28 +0200

    Updated back end
    Updated the database check query
    Updated the send query
    Updated the index.php form

--- index.php
+++ index.php
@@ -12,9 +12,14 @@ require "include/db.php";
         <input type="text" id="sticket" name="sticket"><br>
 
             <label for="edate">Entry date:</label><br>
-        <input type="date" id="Entry" name="Entry-date"
-    value="2021-06-02"
-min="2021-06-02"><br><br>
+            <input type="text" id="edate" name="Entry-date">
+
+
+
+
+        <!--<input type="date" id="Entry" name="Entry-date"
+    value="yyyy-mm-dd"
+    min="2021-06-02"><br><br>-->
 
     <input type="submit" value="Submit" href="include/send.php">
   </form>
